e38499c6af839c0deffb336344ec793515b38206
Mike Kusold <kusold@gmail.com>
2012-06-01 10:41:30 -0400

    Parsing of input text now works. Waiting for Dev server before attempting the GET requests.

--- TabReader/ShiftReaderAppDelegate.h
+++ TabReader/ShiftReaderAppDelegate.h
@@ -18,4 +18,7 @@
 @property (nonatomic, retain) IBOutlet UIWindow *window;
 @property (nonatomic, retain) IBOutlet UITabBarController *tabBarController;
 
+-(NSString*)ParseResults:(NSString*) scannedresult;
+-(void)SendResults;
+
 @end
--- TabReader/ShiftReaderAppDelegate.m
+++ TabReader/ShiftReaderAppDelegate.m
@@ -27,6 +27,9 @@
     reader.readerDelegate = self;
     reader.showsZBarControls = NO;
     reader.supportedOrientationsMask = ZBarOrientationMaskAll;
+    NSLog(@"ALL YOUR BASE ARE BELONG TO US");
+    NSString *test = @"Is this how it works?";
+    NSLog(@"%@", test);
 
     return(YES);
 }
@@ -50,13 +53,44 @@
     ResultsViewController *results = [tabs.viewControllers objectAtIndex: 1];
     UIImage *image = [info objectForKey: UIImagePickerControllerOriginalImage];
     results.resultImage.image = image;
-
+    NSString *test = @"Is this how it works?";
+    NSLog(@"%@", test);
     id <NSFastEnumeration> syms =
     [info objectForKey: ZBarReaderControllerResults];
     for(ZBarSymbol *sym in syms) {
+        NSString *url = [self ParseResults:sym.data];
+        NSLog(@"%@%@", @"URL Returned from ParseResults: ", url);
+        //TODO: edit results.resultText.text to include success or fail
         results.resultText.text = sym.data;
+        NSLog(@"%@", results.resultText.text);
         break;
     }
+}
+
+// Format text into something I can do something with
+- (NSString*) ParseResults: (NSString*) scannedtext {
+    NSLog(@"%@%@", @"ParseResults, input: ", scannedtext);
+    NSString *prefix = [scannedtext substringToIndex:5];
+    NSLog(@"%@%@", @"ParseResults, prefix: ", prefix);
+    if ([prefix isEqualToString: @"shift"]) {
+        NSString *url = [NSString stringWithFormat:@"%@%@%@%@", @"http://localhost/Validate/params?token=", scannedtext, @"&user=", @"iPhone"];
+        NSLog(@"%@%@", @"ParseResults, return: ", url);
+        return url;
+    } else {
+        return nil;
+    }
+}
+
+// Send results to the server
+- (void) SendResults: (NSString*) url {
+    NSMutableURLRequest *request = [NSMutableURLRequest requestWithURL:[NSURL URLWithString:url] 
+                                                        cachePolicy:NSURLRequestReloadIgnoringLocalAndRemoteCacheData timeoutInterval:10];
+    [request setHTTPMethod: @"GET"];
+    NSError *requestError;
+    NSURLResponse *urlResponse = nil;
+    
+    
+    NSData *response1 = [NSURLConnection sendSynchronousRequest:request returningResponse:&urlResponse error:&requestError];
 }
 
 @end
